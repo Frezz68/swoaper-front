456ad2299afacede7c9ab2e3bff0a27bff3d01e9
Alan <alan.jordy1999@gmail.com>
2023-10-19 12:20:47 +0200

    design commit

--- src/components/CardPrestation/CardPrestation.tsx
+++ src/components/CardPrestation/CardPrestation.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import "./CardPrestation.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
 interface CardPrestationProps {
     nomPresta: string;
     prixPresta: number;
+    Icon: IconDefinition;
 }
 
 const CardPrestation = ( props: CardPrestationProps) => {
     return(
         <div className="card">
-             <div className="">
-                <div className="py-14 flex flex-row justify-evenly w-2/3">
-                    <div className="flex flex-col justify-start mr-4">
-                        <h5 className="text-white text-xl font-medium mb-2 text-3xl">{props.nomPresta}</h5>
-                        <p className="text-white font-bold mb-4 text-2xl">
-                            {props.prixPresta.toString()}€
+            <div className="cardPos">
+                <div className="cardItem">
+                    <div className="cardFont">
+                        <h5 className="cardText">{props.nomPresta}</h5>
+                        <p className="cardPrice">
+                            {props.prixPresta.toString()} €
                         </p>
                     </div>
-                    <i className="fa-solid fa-hand-sparkles text-5xl text-white"></i>
+                    <FontAwesomeIcon className="cardIcon" icon={props.Icon} style={{color: "#ffffff",}} />
                 </div>
             </div>
         </div>
