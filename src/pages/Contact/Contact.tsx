384bcf7b15ce001c98875c0a4c1f5280f531e252
Alan <alan.jordy1999@gmail.com>
2024-02-14 11:12:40 +0100

    🎨 style(Contact.scss): add styles for contact form elements to improve UI ✨ feat(Contact.tsx): replace placeholder content with contact form to enhance functionality 🎨 style(button.scss): add styles for tertiary button to improve UI

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Contact.scss";
+import "../../style/button.scss";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the content of the home page.</p>
+    <div className="container-contact">
+      <h1 className="titre">Contactez-nous !</h1>
+      <input className="email" type="text" placeholder="Email" />
+      <input className="objet" type="text" placeholder="Objet" />
+      <textarea className="message" placeholder="Message" />
+      <input className="pieceJointe" type="file"  accept="png,jpg"/>
+      <button className="btn-tertiary">Envoyer</button>
     </div>
   );
 };
